f5bab1b23aebd96a4e34d9798e342970aefa484c
bambamot <71121623+bambamot@users.noreply.github.com>
2021-07-21 23:12:19 +0700

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -51,7 +51,7 @@ describe('Game', () => {
     })
   })
 
-//   describe('nextPlayer', () => {
+  describe('nextPlayer', () => {
 //     it('Sets the current player to be whoever it is not', async () => {
 //       Math.random = () => 0.4
 //       const game = new Game(p1, p2)
@@ -59,7 +59,7 @@ describe('Game', () => {
 //       game.nextPlayer()
 //       expect(game.player).toBe('Nate')
 //     })
-//   })
+  })
 
   describe('hasWinner', () => {
     it('Wins if any row is filled', async () => {
